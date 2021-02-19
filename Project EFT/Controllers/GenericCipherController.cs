8a7a8ba11cf7fd0c34c05aa8a993b06717bd0dba
jgross11 <jgross11@ycp.edu>
2021-02-19 14:06:32 -0500

    Create GenericCipherController.cs
    Created Controller for generic ciphers page

---
+++ Project EFT/Controllers/GenericCipherController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_EFT.Models;
+
+namespace Project_EFT.Controllers
+{
+    public class GenericCipherController : Controller
+    {
+        private readonly ILogger<GenericCipherController> _logger;
+
+        public GenericCipherController(ILogger<GenericCipherController> logger)
+        {
+            _logger = logger;
+        }
+
+        // localhost.../GenericCipher, see Startup.cs for more info
+        public IActionResult GetPage()
+        {
+            return GenericCipher();
+        }
+
+        public IActionResult GenericCipher()
+        {
+            // must set viewdata here, not in initial GetPage
+            ViewData["echo"] = "Hello again, World!";
+
+            // attempts to locate cshtml file with name GenericCipher in GenericCipher folder and Shared folder
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
