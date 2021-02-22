cb6ddae1633c14527cbf357e4dd0cac4f57e91d5
jgross11 <jgross11@ycp.edu>
2021-02-22 17:15:22 -0500

    Create DBConnector.cs
    created class to handle db interactions

---
+++ Project EFT/Database/DBConnector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Project_EFT.Database
+{
+    public class DBConnector
+    {
+        public static MySqlConnection connection;
+        
+        public static bool Init() 
+        {
+            string[] lines = System.IO.File.ReadAllLines("../../info.txt");
+            
+            // dependent on the structure of the file...
+            connection = new MySqlConnection(
+                @"server=" + lines[0] + ";userid=" + lines[1] + ";password=" + lines[2] + ";database=" + lines[3]
+            );
+            connection.Open();
+            return true;
+        }
+
+
+    }
+}
