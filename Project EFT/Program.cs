0cf747addf44b45ebef6bfe0966c702c3889731e
jgross11 <jgross11@ycp.edu>
2021-02-22 17:16:24 -0500

    Added DB connectivity
    title says it all

--- Project EFT/Program.cs
+++ Project EFT/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Project_EFT.Database;
 
 namespace Project_EFT
 {
@@ -13,6 +14,9 @@ namespace Project_EFT
     {
         public static void Main(string[] args)
         {
+            // connect to database
+            DBConnector.Init();
+
             CreateHostBuilder(args).Build().Run();
         }
 
