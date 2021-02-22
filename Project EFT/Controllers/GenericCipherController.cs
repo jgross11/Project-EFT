96d02ea62b85e182b1f71dc1a32af1c1e181dea9
jgross11 <jgross11@ycp.edu>
2021-02-22 08:51:06 -0500

    Update GenericCipherController.cs
    added decryption routing logic

--- Project EFT/Controllers/GenericCipherController.cs
+++ Project EFT/Controllers/GenericCipherController.cs
@@ -39,14 +39,40 @@ namespace Project_EFT.Controllers
             string ciphertext = new CaesarCipher(Request.Form["alphabet"], 0, int.Parse(Request.Form["encryptShiftAmount"]), 1).Encrypt(Request.Form["plainTextInput"]);
             
             // store ciphertext in view map
-            ViewData["ciphertext"] = ciphertext;
+            ViewData["cipherTextInput"] = ciphertext;
 
             // store other form contents to populate form 
             ViewData["alphabet"] = Request.Form["alphabet"];
+            ViewData["decryptShiftAmount"] = Request.Form["decryptShiftAmount"];
             ViewData["encryptShiftAmount"] = Request.Form["encryptShiftAmount"];
             ViewData["plainTextInput"] = Request.Form["plainTextInput"];
 
             // refresh page
+            return View("GenericCipher");
+        }
+
+        [HttpPost]
+        public IActionResult decrypt() 
+        {
+
+            // debug form contents
+            Debug.WriteLine("From request");
+            Debug.WriteLine(Request.Form["alphabet"]);
+            Debug.WriteLine(Request.Form["decryptShiftAmount"]);
+            Debug.WriteLine(Request.Form["cipherTextInput"]);
+
+            // do decryption
+            string plaintext = new CaesarCipher(Request.Form["alphabet"], int.Parse(Request.Form["decryptShiftAmount"]), 0, 1).Decrypt(Request.Form["cipherTextInput"])[0];
+
+            // store plain text in view map
+            ViewData["plainTextInput"] = plaintext;
+
+            // store other form contents to populate form 
+            ViewData["alphabet"] = Request.Form["alphabet"];
+            ViewData["encryptShiftAmount"] = Request.Form["encryptShiftAmount"];
+            ViewData["decryptShiftAmount"] = Request.Form["decryptShiftAmount"];
+            ViewData["cipherTextInput"] = Request.Form["cipherTextInput"];
+
             return View("GenericCipher");
         }
 
