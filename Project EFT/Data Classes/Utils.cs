75f29b9e700ae3ed95c49c095f2ecd32d2ce3844
jgross11 <jgross11@ycp.edu>
2021-02-27 22:20:45 -0500

    Create Utils.cs
    created class to contain general backend helper functions

---
+++ Project EFT/Data Classes/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Project_EFT.Data_Classes
+{
+    public static class Utils
+    {
+        public static void SetComplexObject<T>(this ISession session, string keyName, T value) 
+        {
+            session.SetString(keyName, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetComplexObject<T>(this ISession session, string keyName) 
+        {
+            string value = session.GetString(keyName);
+            return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+        }
+
+        public static bool ContainsKey(this ISession session, string keyName)
+        {
+            return session.GetString(keyName) != null;
+        }
+    }
+}
