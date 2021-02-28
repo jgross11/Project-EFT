b281948320c51c65232ccdc0876d1cba78145230
jgross11 <jgross11@ycp.edu>
2021-02-27 22:18:35 -0500

    Created user data classes
    abstract user, standard user, and admin

---
+++ Project EFT/Data Classes/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_EFT.Data_Classes
+{
+    public abstract class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+
+        public User(string username, string password, string email, int id) 
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Id = id;
+        }
+
+        public User(string username, string password, string email) 
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Id = 0;
+        }
+
+        public User() 
+        { 
+        
+        }
+    }
+}
