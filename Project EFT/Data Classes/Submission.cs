d879ec3496aecae155c4cdcca9fc41014cd7f000
jgross11 <jgross11@ycp.edu>
2021-02-27 22:18:49 -0500

    Created submission data classes
    generic and answer-specific

---
+++ Project EFT/Data Classes/Submission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_EFT.Data_Classes
+{
+    public class Submission
+    {
+        public string Content { get; set; }
+        public DateTime SubmissionDate { get; set; }
+
+        public Submission(string content, DateTime submissionDate) 
+        {
+            Content = content;
+            SubmissionDate = submissionDate;
+        }
+    }
+}
