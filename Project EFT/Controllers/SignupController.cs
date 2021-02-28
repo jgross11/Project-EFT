ddf8d77d08e7f9de61d169785866a6cee7d3778d
jgross11 <jgross11@ycp.edu>
2021-02-27 22:23:22 -0500

    Create SignupController.cs
    created controller to handle GETting of singup page / signup form submission

---
+++ Project EFT/Controllers/SignupController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Project_EFT.Data_Classes;
+using Project_EFT.Database;
+
+namespace Project_EFT.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitSignup() 
+        {
+            string username = Request.Form["username"];
+            string email = Request.Form["email"];
+            string password = Request.Form["password"];
+            Debug.WriteLine(username);
+            Debug.WriteLine(email);
+            Debug.WriteLine(password);
+            StandardUser newUser = new StandardUser(username, password, email);
+            // TODO conditionals
+            DBConnector.InsertNewUser(newUser);
+
+            HttpContext.Session.SetComplexObject("userInfo", newUser);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
