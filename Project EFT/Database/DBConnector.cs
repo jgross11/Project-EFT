190d2a56e7a4c46736fb5bd5f7cd9934913b9a78
andersonjackc <60368246+andersonjackc@users.noreply.github.com>
2021-03-24 12:33:20 -0400

    Update DBConnector.cs

--- Project EFT/Database/DBConnector.cs
+++ Project EFT/Database/DBConnector.cs
@@ -343,18 +343,6 @@ namespace Project_EFT.Database
 
         }
 
-        public static bool GetProblemCorrectValueByUserAndProblemID(int userID, int problemID)
-        {
-            MySqlCommand command = MakeCommand("SELECT AnswerSubmissions_IsCorrect FROM AnswerSubmissions WHERE User_ID = @user_id AND AnswerSubmissions_ProblemID = @problem_id AND AnswerSubmissions_IsCorrect = TRUE");
-            command.Parameters.AddWithValue("@user_id", userID);
-            command.Parameters.AddWithValue("@problem_id", problemID);
-            command.Prepare();
-            MySqlDataReader reader = command.ExecuteReader();
-            bool result = reader.Read();
-            connection.Close();
-
-            return result;
-        }
 
         public static int InsertNewUser(StandardUser user)
         {
