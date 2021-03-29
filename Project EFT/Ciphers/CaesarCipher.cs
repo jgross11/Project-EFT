ec1f95d2291509a1f3cba673fb034900addc5f9b
jgross11 <jgross11@ycp.edu>
2021-03-29 13:03:49 -0400

    Cipher class refactoring
    Moved cipher classes to own namespace for startup type name generation convenience

---
+++ Project EFT/Ciphers/CaesarCipher.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Project_EFT.Ciphers.Options;
+
+namespace Project_EFT.Ciphers
+{
+    // data class representing a Caesar Cipher
+    public class CaesarCipher : Cipher
+    {
+        public override string Name { get { return "Caesar Cipher"; } }
+        private const int ShiftIndex = 2;
+
+        public CaesarCipher(int numSols) : base(numSols)
+        {
+            
+        }
+
+        public CaesarCipher() : base() { }
+
+        public override void GenerateForms() 
+        {
+            EncryptionFormOptions.Add(new NumberFieldOption("encryptionShiftAmount", "Enter shift amount", 3));
+            DecryptionFormOptions.Add(new NumberFieldOption("decryptionShiftAmount", "Enter shift amount", 3));
+        }
+
+        public override string Encrypt()
+        {
+            string ciphertext = "";
+            string plaintext = (string)EncryptionFormOptions[InputIndex].GetValue();
+            string alphabet = (string)EncryptionFormOptions[AlphabetIndex].GetValue();
+            int encryptionShiftAmount = (int)EncryptionFormOptions[ShiftIndex].GetValue();
+
+            foreach (char c in plaintext) 
+            {
+                // ignore characters not in alphabet (spaces, periods, etc.)
+                int index = alphabet.IndexOf(c);
+
+                if (index == -1)
+                {
+                    ciphertext += c;
+                }
+                // otherwise, do shift
+                else
+                {
+                    int newPosition = (((index + encryptionShiftAmount) % alphabet.Length) + alphabet.Length) % alphabet.Length;
+                    ciphertext += alphabet[newPosition];
+                }
+            }
+            // TODO store in appropriate option
+            DecryptionFormOptions[InputIndex].SetValue(ciphertext);
+            return ciphertext;
+        }
+
+        public override string[] Decrypt()
+        {
+            string[] plaintexts = new string[NumSolutionsToReturn];
+            string ciphertext = (string)DecryptionFormOptions[InputIndex].GetValue();
+            string alphabet = (string)DecryptionFormOptions[AlphabetIndex].GetValue();
+            int decryptionShiftAmount = (int)DecryptionFormOptions[ShiftIndex].GetValue();
+            foreach (char c in ciphertext) 
+            {
+                int index = alphabet.IndexOf(c);
+                if (index == -1) 
+                {
+                    plaintexts[0] += c;
+                }
+                else 
+                {
+                    int newPosition = (((index - decryptionShiftAmount) % alphabet.Length) + alphabet.Length) % alphabet.Length;
+                    plaintexts[0] += alphabet[newPosition];
+                }
+            }
+            // TODO store in appropriate option
+            EncryptionFormOptions[InputIndex].SetValue(plaintexts[0]);
+            return plaintexts;
+        }
+    }
+}
---
+++ Project EFT/Ciphers/Cipher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project_EFT.Ciphers.Options;
+
+namespace Project_EFT.Ciphers
+{
+    public abstract class Cipher
+    {
+        public int NumSolutionsToReturn {get; set;}
+        public virtual string Name { get; set; }
+        public string[] outputs { get; set; }
+        public List<Option> EncryptionFormOptions;
+        public List<Option> DecryptionFormOptions;
+        public abstract void GenerateForms();
+        public abstract string Encrypt();
+        public abstract string[] Decrypt();
+        public const int InputIndex = 0;
+        public const int AlphabetIndex = 1;
+
+        public Cipher(int numSols) 
+        {
+            NumSolutionsToReturn = numSols;
+            Init();
+        }
+
+        public Cipher() {
+            NumSolutionsToReturn = 1;
+            Init();
+        }
+
+        public void Init() 
+        {
+            Name = "default";
+            EncryptionFormOptions = new List<Option>();
+            EncryptionFormOptions.Add(new TextAreaOption("plaintextInput"));
+            EncryptionFormOptions.Add(new TextBoxOption("encryptionAlphabet", "Enter alphabet", "abcdefghijklmnopqrstuvwxyz"));
+            DecryptionFormOptions = new List<Option>();
+            DecryptionFormOptions.Add(new TextAreaOption("ciphertextInput"));
+            DecryptionFormOptions.Add(new TextBoxOption("decryptionAlphabet", "Enter alphabet", "abcdefghijklmnopqrstuvwxyz"));
+            GenerateForms();
+            outputs = new string[NumSolutionsToReturn];
+        }
+
+    }
+}
