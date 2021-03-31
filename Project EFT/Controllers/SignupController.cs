3eb4bf6edc6f300c3fbb926e34383cac16b6c187
andersonjackc <60368246+andersonjackc@users.noreply.github.com>
2021-03-31 10:58:27 -0400

    Creates a User Specific Submission Table on Signup

--- Project EFT/Controllers/SignupController.cs
+++ Project EFT/Controllers/SignupController.cs
@@ -37,6 +37,7 @@ namespace Project_EFT.Controllers
                 if (result != -1)
                 {
                     newUser.Id = result;
+                    DBConnector.CreateUserSubmissionTable(newUser.Id);
                     Mailer.SendWelcomeEmail(newUser);
                     HttpContext.Session.SetComplexObject("userInfo", newUser);
                     return RedirectToAction("Index", "Home");
