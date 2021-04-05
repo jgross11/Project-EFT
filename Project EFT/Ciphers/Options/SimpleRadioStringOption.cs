800c6664eb1dcd54c9fd94f72c859c41bdab97b0
jgross11 <jgross11@ycp.edu>
2021-04-04 22:51:43 -0400

    Create SimpleRadioStringOption.cs
    Created option representing a simple string, to be used (probably) exclusively within a RadioOptionsSet

---
+++ Project EFT/Ciphers/Options/SimpleRadioStringOption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_EFT.Ciphers.Options
+{
+    public class SimpleRadioStringOption : Option
+    {
+        public SimpleRadioStringOption(string fieldName) : base(fieldName, null){}
+
+        public override string GetHTML()
+        {
+            return FieldName;
+        }
+
+        public override object GetValue()
+        {
+            return null;
+        }
+
+        public override void SetValue(string newVal)
+        {
+            
+        }
+    }
+}
