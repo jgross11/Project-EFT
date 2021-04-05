ef44cd183c214fe4bf68fefb1ed9dcfdffc4f02b
jgross11 <jgross11@ycp.edu>
2021-04-04 22:48:37 -0400

    Added error messages
    Implemented generation, formatting, etc

--- Project EFT/Ciphers/Cipher.cs
+++ Project EFT/Ciphers/Cipher.cs
@@ -14,10 +14,11 @@ namespace Project_EFT.Ciphers
         public List<Option> EncryptionFormOptions;
         public List<Option> DecryptionFormOptions;
         public abstract void GenerateForms();
-        public abstract string Encrypt();
-        public abstract string[] Decrypt();
+        public abstract void Encrypt();
+        public abstract void Decrypt();
         public const int InputIndex = 0;
         public const int AlphabetIndex = 1;
+        public const string standardAlphabet = "abcdefghijklmnopqrstuvwxyz";
 
         public Cipher(int numSols) 
         {
@@ -35,12 +36,24 @@ namespace Project_EFT.Ciphers
             Name = "default";
             EncryptionFormOptions = new List<Option>();
             EncryptionFormOptions.Add(new TextAreaOption("plaintextInput"));
-            EncryptionFormOptions.Add(new TextBoxOption("encryptionAlphabet", "Enter alphabet", "abcdefghijklmnopqrstuvwxyz"));
+            EncryptionFormOptions.Add(new TextBoxOption("encryptionAlphabet", "Enter alphabet", standardAlphabet));
             DecryptionFormOptions = new List<Option>();
             DecryptionFormOptions.Add(new TextAreaOption("ciphertextInput"));
-            DecryptionFormOptions.Add(new TextBoxOption("decryptionAlphabet", "Enter alphabet", "abcdefghijklmnopqrstuvwxyz"));
+            DecryptionFormOptions.Add(new TextBoxOption("decryptionAlphabet", "Enter alphabet", standardAlphabet));
             GenerateForms();
             outputs = new string[NumSolutionsToReturn];
+        }
+
+        public void ResetErrors()
+        {
+            foreach (Option opt in EncryptionFormOptions)
+            {
+                opt.ErrorMessage = null;
+            }
+            foreach (Option opt in DecryptionFormOptions)
+            {
+                opt.ErrorMessage = null;
+            }
         }
 
         public int Mod(int n, int mod) 
--- Project EFT/Ciphers/Options/Option.cs
+++ Project EFT/Ciphers/Options/Option.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Project_EFT.Ciphers.Options
 {
@@ -9,19 +11,39 @@ namespace Project_EFT.Ciphers.Options
     {
         public string FieldName { get; set; }
         public string DisplayName { get; set; }
+        public string ErrorMessage { get; set; }
 
         public Option(string fieldName, string displayName) 
         {
             FieldName = fieldName;
             DisplayName = displayName;
+            ErrorMessage = null;
+        }
+
+        public Option(string fieldName, string displayName, string errorMessage)
+        {
+            FieldName = fieldName;
+            DisplayName = displayName;
+            ErrorMessage = errorMessage;
         }
 
         public abstract string GetHTML();
+        public virtual string GetErrorHTML()
+        {
+            return String.Format("<div class = 'error' id='{0}-error'>{1}</div>", FieldName, ErrorMessage);
+        }
 
         // https://bit.ly/3u2ymVn
         // lines 63 and 64
         public abstract void SetValue(string newVal);
         public abstract object GetValue();
+        public virtual void ObtainValueFromForm(IFormCollection form)
+        {
+            if (FieldName != null)
+            {
+                SetValue(form[FieldName].ToString());
+            }
+        }
     }
 
     public abstract class Option<T> : Option
@@ -29,6 +51,11 @@ namespace Project_EFT.Ciphers.Options
         public T Value;
 
         public Option(string fieldName, string displayName, T value) : base(fieldName, displayName)
+        {
+            Value = value;
+        }
+
+        public Option(string fieldName, string displayName, string errorMessage, T value) : base(fieldName, displayName, errorMessage)
         {
             Value = value;
         }
